0a045f3a652173046c854122a9c071b83c7b2725
Evgheniy Scofield <jenea9833@gmail.com>
2024-06-02 18:50:59 +0300

    updated app1.py

--- app1.py
+++ app1.py
@@ -27,7 +27,7 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Load dataset
 st.title("Clasificarea persoanelor dupa venit")
-st.write("Devs: Eugeniu Casian&Virgiliu Plesca/SD-231M .")
+st.write("Devs: Eugeniu Casian&Virgiliu Plesca/SD-231M/UTM.")
 
 
 # Function to load and preprocess the dataset
@@ -35,9 +35,6 @@ def load_data(file):
     data = pd.read_csv(file, delimiter=';')
     data.dropna(inplace=True)
     
-
-    return data
-
     # Encode categorical variables except the target variable 'income'
     label_encoders = {}
     for column in data.select_dtypes(include=['object']).columns:
@@ -49,7 +46,7 @@ def load_data(file):
             label_encoders[column] = LabelEncoder()
             data[column] = label_encoders[column].fit_transform(data[column])
     
-   # return data, label_encoders
+    return data, label_encoders
 
 # Streamlit UI
 st.write("## Upload your dataset")
